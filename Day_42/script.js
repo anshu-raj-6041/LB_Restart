729769c62e3c00296232a721b07a474b7b7937ba
anshu-raj-6041 <anshu604125@gmail.com>
2025-09-01 09:05:48 +0530

    JS Basics

---
+++ Day_42/script.js
@@ -0,0 +1,19 @@
+// console.log("Hello_2");
+
+let a = 25;
+a = "Anshu" // dynamic typing
+// let a = "Modi";
+// let a = true;
+console.log(a);
+
+// wrong
+// const num = 10;
+// num = 90;
+
+let b;
+console.log(b); // undefined
+
+let c = null;
+console.log(c);
+
+
